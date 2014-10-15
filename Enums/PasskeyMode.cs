04086ddf75eaa993c0fc6c8f4ca1b5f79684464d
jbeechum <jbeechum@dataconnectcorp.com>
2014-10-15 13:58:09 -0600

    New enum for mode of service.  (Training, QA, Production)

---
+++ Enums/PasskeyMode.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <summary>
+//   The passkey mode.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PasskeyCoreSDK.Enums
+{
+    /// <summary>
+    /// The passkey mode.
+    /// </summary>
+    public enum PasskeyMode
+    {
+        /// <summary>
+        /// Certification Mode
+        /// </summary>
+        Training = 1, 
+
+        /// <summary>
+        /// QA Mode
+        /// </summary>
+        QA = 2, 
+
+        /// <summary>
+        /// Production Mode
+        /// </summary>
+        Production = 3
+    }
+}
